48ba9a2665ba6f1c0be8781709315470d6183ce3
Mohamed Alganzory <blogmidoelg@gmail.com>
2021-10-07 03:13:47 +0800

    basic styling done, next: mistakes, more styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import React, { useState, useEffect, useRef, useCallback } from "react";
 import { TimerComponent } from "./TimerComponent";
 import randomWords from "random-words";
-import words from "random-words";
 
 const FASTEST_WPM = 100;
-
-function App() {
-  const [wordsArray, setWordsArray] = useState(null);
-  const [minutes, setMinutes] = useState(1);
-  const [inputValue, setInputValue] = useState("");
-  const arrayIdx = useRef(0);
-  const wordIdx = useRef(0);
-  const [started, setStarted] = useState(false);
-  const [scores, setScores] = useState({
+const INITIAL_STATE = {
+  minutes: 1,
+  inputValue: "",
+  started: null,
+  scores: {
     charsCount: 0,
     wordsCount: 0,
     mistakes: 0,
-  });
+  },
+};
 
+const REFS_INITIAL = {
+  arrayIdx: 0,
+  wordIdx: 0,
+  isExpectedSpace: false,
+  isBackSpace: false,
+  spanIndex: 0,
+  prevHeight: 0,
+};
+function App() {
+  const [{ minutes, inputValue, started, scores }, setState] = useState({
+    ...INITIAL_STATE,
+  });
+  const [wordsArray, setWordsArray] = useState(null);
+  // const [minutes, setMinutes] = useState(1);
+  // const [inputValue, setInputValue] = useState("");
+  const arrayIdx = useRef(0);
+  const wordIdx = useRef(0);
+  // const slideCount = useRef(false);
+  // const [started, setStarted] = useState(null);
+  // const [scores, setScores] = useState({
+  //   charsCount: 0,
+  //   wordsCount: 0,
+  //   mistakes: 0,
+  // });
+
+  const [incorrectLetters, setIncorrectLetters] = useState(new Map());
+  const [restart, setRestart] = useState(0);
   const sentenceContainer = useRef();
   const inputRef = useRef();
   const scoresRef = useRef();
@@ -25,38 +48,78 @@ function App() {
   const isExpectedSpace = useRef(false);
   const isBackSpace = useRef(false);
   const spanIndex = useRef(0);
-  const prevHeight = useRef (0);
-  useEffect(() => {
-    setWordsArray(randomWords({ exactly: FASTEST_WPM * minutes }));
-    inputRef.current.focus();
-  }, [minutes]);
+  const prevHeight = useRef(0);
 
+  const resetRefs = () => {
+   
+    // Object.keys(REFS_INITIAL).forEach ((key, index) => {
+    //   console.log([{]);
+    // })
+    arrayIdx.current = REFS_INITIAL.arrayIdx;
+    wordIdx.current = REFS_INITIAL.wordIdx;
+    isExpectedSpace.current = REFS_INITIAL.isExpectedSpace;
+    isBackSpace.current = REFS_INITIAL.isBackSpace;
+    spanIndex.current = REFS_INITIAL.spanIndex;
+    prevHeight.current = REFS_INITIAL.prevHeight;
+  };
+  useEffect(() => {
+    // console.log("useEffect where we generate words");
+    setWordsArray(() => {
+      // console.log("generating new words...");
+      // const newWords  = randomWords({ exactly: FASTEST_WPM * minutes });
+      // console.log(newWords)
+
+      return randomWords({ exactly: FASTEST_WPM * minutes });
+    });
+    inputRef.current.focus();
+    // inputRef.current.disabled = "false"
+  }, [minutes, restart]);
+
+  // useEffect(() => {
+  //   console.log("NEW RANDOS: " + wordsArray);
+  // }, [wordsArray]);
   useEffect(() => {
     if (wordsArray == null) return;
+    if (started == null) return;
     // console.log(wordsArray);
+    // slideCount.current = false;
+    console.log("useeffect where we deal with input");
+    // console.log (incorrectLetters);
     const recentlyInputLetter = inputValue[inputValue.length - 1];
     const currentWord = wordsArray[arrayIdx.current];
     let prevWord = wordsArray[arrayIdx.current <= 0 ? 0 : arrayIdx.current - 1];
     let currentLetter = wordsArray[arrayIdx.current][wordIdx.current];
+    let spanId = isExpectedSpace.current
+      ? "space" + spanIndex.current
+      : currentLetter + spanIndex.current;
+    let currentCharacterSpan = document.getElementById(spanId);
+    if (currentCharacterSpan == null) return;
+    const currentHeight = currentCharacterSpan.getBoundingClientRect().y;
+    // console.log(currentHeight, prevHeight.current)
     // let prevLetter = prevWord[prevWord.length-1];
 
     //  spanIndex= 2*arrayIdx.current+wordIdx.current;
 
-    let spanId = isExpectedSpace.current
-      ? "space" + spanIndex.current
-      : currentLetter + spanIndex.current;
-    const currentCharacterSpan = document.getElementById(spanId)
-    const currentHeight = currentCharacterSpan.getBoundingClientRect().y;
-    console.log(currentHeight, prevHeight.current)
-    if (currentHeight> prevHeight.current) {
+    // console.log(currentHeight, prevHeight.current);
+    if (currentHeight > prevHeight.current) {
       const newSlicedArray = wordsArray.slice(
         arrayIdx.current,
         wordsArray.length
       );
       // console.log(newSlicedArray);
+      // console.log("setSLICED");
       setWordsArray(newSlicedArray);
+      // setWordsArray(newSlicedArray);
       arrayIdx.current = 0;
       spanIndex.current = 0;
+      // slideCount.current = true;
+      // sentenceContainer.current.className = "original-text slide"
+      //  sentenceContainer.current.style.transform = "translateY(100ppx)";
+      //  console.log("slide")
+    } else {
+      // console.log("slide Count should be false")
+      // slideCount.current = false;
+      // sentenceContainer.current.className = "original-text"
     }
     prevHeight.current = currentHeight;
     // console.log(currentCharacterSpan.getBoundingClientRect())
@@ -97,36 +160,59 @@ function App() {
       //  console.table(recentlyInputLetter, currentLetter, currentWord)
       if (isBackSpace.current) {
         isBackSpace.current = false;
-      } else
-        setScores((prevScores) => ({
-          ...prevScores,
-          mistakes: prevScores.mistakes + 1,
-        }));
+        return;
+      }
+      // } else
+      //   setScores((prevScores) => ({
+      //     ...prevScores,
+      //     mistakes: prevScores.mistakes + 1,
+      //   }));
 
       currentCharacterSpan.className = "letter-incorrect";
-      
+      // console.log(currentCharacterSpan.id);
       // if (isExpectedSpace.current) {
       //   // setIncorrectLetter(" ");
       // } else setIncorrectLetter(currentLetter);
       // //  console.log(incorrectLetter.current);
 
+      setIncorrectLetters((prevIncorrectLetters) => {
+        let newIncorrectLetters = prevIncorrectLetters;
+
+        if (isExpectedSpace.current) currentLetter = "space";
+
+        if (newIncorrectLetters.has(currentLetter)) {
+          let oldValue = newIncorrectLetters.get(currentLetter);
+          newIncorrectLetters.set(currentLetter, (oldValue += 1));
+          return newIncorrectLetters;
+        }
+        newIncorrectLetters.set(currentLetter, 1);
+
+        return newIncorrectLetters;
+      });
+
       return;
     }
 
     if (isSpaceValid()) {
-      setScores((prevScores) => ({
-        ...prevScores,
-        charsCount: prevScores.charsCount + 1,
+      setState((prevState) => ({
+        ...prevState,
+        scores: {
+          ...prevState.scores,
+          charsCount: prevState.scores.charsCount + 1,
+        },
       }));
+      // setScores((prevScores) => ({
+      //   ...prevScores,
+      //   charsCount: prevScores.charsCount + 1,
+      // }));
       // setCorrectStuff(
       //   (prevCorrectStuff) => (prevCorrectStuff += recentlyInputLetter)
       // );
       currentCharacterSpan.className = "letter-correct";
       isExpectedSpace.current = false;
-      setInputValue("");
+      setState((prevState) => ({ ...prevState, inputValue: "" }));
+      // setInputValue("");
       spanIndex.current++;
-
-      console.log(currentCharacterSpan.getBoundingClientRect().left);
 
       return;
     }
@@ -142,11 +228,17 @@ function App() {
         isExpectedSpace.current = false;
         wordIdx.current++;
       }
-
-      setScores((prevScores) => ({
-        ...prevScores,
-        charsCount: prevScores.charsCount + 1,
+      setState((prevState) => ({
+        ...prevState,
+        scores: {
+          ...prevState.scores,
+          charsCount: prevState.scores.charsCount + 1,
+        },
       }));
+      // setScores((prevScores) => ({
+      //   ...prevScores,
+      //   charsCount: prevScores.charsCount + 1,
+      // }));
       // setCorrectStuff(
       //   (prevCorrectStuff) => (prevCorrectStuff += recentlyInputLetter)
       // );
@@ -154,7 +246,7 @@ function App() {
       currentCharacterSpan.className = "letter-correct";
       spanIndex.current++;
     }
-  }, [inputValue, wordsArray]);
+  }, [inputValue, wordsArray, started]);
 
   // useEffect(() => {
   //   if (correctStuff[correctStuff.length - 1] === " ")
@@ -165,24 +257,49 @@ function App() {
   // }, [correctStuff]);
 
   const onTimerFinish = useCallback(() => {
-    setStarted(false);
+    setState((prevState) => ({ ...prevState, started: false }));
+    // setStarted(false);
     inputRef.current.blur();
 
     return [true, 0];
   }, []);
 
-  const timerComponent = useCallback(function ({ remainingTime, elapsedTime }) {
-    timeElapsed.current = elapsedTime;
-    return (
-      <div className="timer">
-        <div className="value">{remainingTime}</div>
-        <div className="text">seconds</div>
-      </div>
-    );
+  const handleRestart = useCallback(() => {
+    // alert("RESTART");
+    resetRefs();
+    setState({ ...INITIAL_STATE });
+    setRestart((prevRestart) => (prevRestart += 1));
+    //  setRestart(true);
+    //  setScores ({});
+    //  setStarted ();
+    //  setInputValue("")
   }, []);
+  const timerComponent = useCallback(
+    function ({ remainingTime, elapsedTime }) {
+      timeElapsed.current = elapsedTime;
+      return started !== false ? (
+        <div className="timer">
+          <div className="value">{remainingTime}</div>
+          <div className="text">seconds</div>
+        </div>
+      ) : (
+        <div className="timer">
+          <div style={{ fontSize: ".75rem", color: "#777" }}>
+            {remainingTime} seconds
+          </div>
+          <div onClick={handleRestart} className="restart">
+            Restart
+          </div>
+        </div>
+      );
+    },
+    [started, handleRestart]
+  );
 
   function handleChange({ target }) {
-    setInputValue(target.value);
+    // setInputValue(target.value);
+    console.log("handleChange");
+    setState((prevState) => ({ ...prevState, inputValue: target.value }));
   }
 
   function handleKey(e) {
@@ -195,18 +312,25 @@ function App() {
   }
 
   function startGame() {
-    setStarted(true);
-    setScores({ wordsCount: 0, charsCount: 0, mistakes: 0 });
-    setInputValue("");
+    setState({ ...INITIAL_STATE });
+    setState((prevState) => ({ ...prevState, started: true }));
+    // setStarted(true);
+    // setScores({ wordsCount: 0, charsCount: 0, mistakes: 0 });
+    // setInputValue("");
   }
 
-  const WPM = timeElapsed.current ===0? 0: scores.charsCount / 5 / (timeElapsed.current / 60.0) ;
-  const accuracy = spanIndex.current === 0? 0: Math.max(
-    (scores.charsCount - scores.mistakes) / (spanIndex.current),
-    0
-  ) * 100;
+  const WPM =
+    timeElapsed.current === 0
+      ? 0
+      : scores.charsCount / 5 / (timeElapsed.current / 60.0);
+  const accuracy =
+    spanIndex.current === 0
+      ? 0
+      : Math.max((scores.charsCount - scores.mistakes) / spanIndex.current, 0) *
+        100;
 
   function wordsArrayJSX() {
+    // console.log("called: ", wordsArray);
     return wordsArray == null
       ? ""
       : wordsArray
@@ -219,7 +343,6 @@ function App() {
               format = character + idx;
             }
 
-
             return (
               <span className="letter" id={format} key={format}>
                 {character}
@@ -227,53 +350,92 @@ function App() {
             );
           });
   }
+
   return (
-    <>
+    <div className="container">
       <div className="header">
         <div className="scores" ref={scoresRef}>
-          <div className="score" style={{ color: "red" }}>
+          {/* <div className="score" >
             {" "}
             Mistakes: {scores.mistakes}{" "}
+          </div> */}
+          <div className="score">
+            <label form="CPM">CPM</label>
+            <input
+            disabled
+              id="CPM"
+              className="score-input"
+              value={scores.charsCount}
+            ></input>
+          
           </div>
-          <div className="score" style={{ color: "green" }}>
-            {" "}
-            CPM: {scores.charsCount}{" "}
+          <div className="score">
+            <label form="WPM">WPM</label>
+            <input
+            disabled
+              id="WPM"
+              className="score-input"
+              value={WPM.toFixed()}
+            ></input>
+            </div>
+          <div className="score">
+            <label form="accuracy">Accuracy</label>
+            <input
+            disabled
+              id="accuracy"
+              className="score-input"
+              value={accuracy.toFixed() + "%"}
+            ></input>
+          
           </div>
-          <div className="score" style={{ color: "green" }}>
-            {" "}
-            WPM: {WPM.toFixed()}{" "}
-          </div>
-          <div className="score" style={{ color: "green" }}>
-            {" "}
-            Accuracy: {accuracy.toFixed()}%
-          </div>
+
         </div>
         <div className="timer-container">
-          <TimerComponent
-            className
-            onTimerFinish={onTimerFinish}
-            timerComponent={timerComponent}
-            startPlaying={started}
-            duration={60}
-          />
+          <div className="timer-component">
+            <TimerComponent
+            
+              onTimerFinish={onTimerFinish}
+              timerComponent={timerComponent}
+              startPlaying={started}
+              duration={2}
+            />
+          </div>
         </div>
       </div>
 
-      <div className="paragraph-container noselect" ref={sentenceContainer}>
-        <div key={wordsArray ? wordsArray[0] : 0} className="original-text">
+      <div className="paragraph-container noselect">
+        <div
+          key={wordsArray ? wordsArray[0] : 0}
+          className={"original-text"}
+          ref={sentenceContainer}
+        >
           {wordsArrayJSX()}
           <div className="fade"></div>
         </div>
 
         <input
+          style={started === false ? { display: "none" } : {}}
           className="typing-input"
           onKeyDown={handleKey}
           value={inputValue}
           ref={inputRef}
           onChange={handleChange}
+          autoCapitalize = "none"
+          autoComplete = "off"
+          spellCheck ="false"
+          autoCorrect="off"
         ></input>
+
+        <button
+          style={started !== false ? { display: "none" } : {}}
+          className={"typing-input disabled"}
+          onClick={handleRestart}
+        >
+          Restart
+        </button>
       </div>
-    </>
+     
+    </div>
   );
 }
 
